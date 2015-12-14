76b2b83b892fc6d2d5b4bde7b87f7b250a1e623e
Gareth Robins <gareth.robins@gmail.com>
2015-12-14 22:01:45 +1300

    updated pins again

--- playmp3.py
+++ playmp3.py
@@ -9,7 +9,7 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(4, GPIO.IN)
 
 while True:
-    if (GPIO.input(25)== False):
+    if (GPIO.input(4)== False):
         os.system('mpg123 -q mp3/jinglebells.mp3 &')
 
     sleep(0.1);
