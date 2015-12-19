51dd802ff9b3ced18849b659e9faa16774c83ffd
Gareth Robins <gareth.robins@gmail.com>
2015-12-19 20:04:05 +1300

    added ability to randomly choose mp3 in directory

--- playmp3.py
+++ playmp3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import os
+import os, random
 from time import sleep
 
 import RPi.GPIO as GPIO
@@ -8,8 +8,13 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(4, GPIO.IN)
 
+def rndmp3 ():
+    randomfile = random.choice(os.listdir("mp3"))
+    file = ' mp3/'+ randomfile
+    os.system ('mpg123' + file)
+
 while True:
     if (GPIO.input(4)== True):
-        os.system('mpg123 mp3/jinglebells.mp3')
+        rndmp3 ()
 
     sleep(0.1);
